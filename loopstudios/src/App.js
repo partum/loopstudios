ff38f5407bd0351cbc352e4d6bfcab83a7cda98b
partum <jarringt@uccs.edu>
2021-04-16 17:00:51 -0600

    finished header

--- loopstudios/src/App.js
+++ loopstudios/src/App.js
@@ -1,15 +1,36 @@
 import logo from "./images/logo.svg";
 import vrGuy from "./images/desktop/image-interactive.jpg";
+import curiosity from "./images/desktop/image-curiosity.jpg";
+import deep from "./images/desktop/image-deep-earth.jpg";
+import fisheye from "./images/desktop/image-fisheye.jpg";
+import above from "./images/desktop/image-from-above.jpg";
+import grid from "./images/desktop/image-grid.jpg";
+import night from "./images/desktop/image-night-arcade.jpg";
+import pocket from "./images/desktop/image-pocket-borealis.jpg";
+import soccer from "./images/desktop/image-soccer-team.jpg";
+import facebook from "./images/icon-facebook.svg";
+import twitter from "./images/icon-twitter.svg";
+import pinterest from "./images/icon-pinterest.svg";
+import instagram from "./images/icon-instagram.svg";
 import "./App.css";
 import "./style.css";
 
 function App() {
   return (
-    <body>
+    <div id="main-content">
       <Header />
       <Leader />
       <Creations />
-    </body>
+      <Footer />
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://www.github.com/partum/loopstudios">Julia Arrington</a>.
+      </div>
+    </div>
   );
 }
 
@@ -20,7 +41,9 @@ function Header() {
         <img src={logo} className="App-logo" alt="logo" />
         <NavLinks />
       </nav>
-      <h1>Imersive Experiences That Deliver</h1>
+      <div id="title">
+        <h1>Immersive experiences that deliver</h1>
+      </div>
     </header>
   );
 }
@@ -63,25 +86,52 @@ function Leader() {
 
 function Creations() {
   const projects = [
-    { id: 1, title: "Deep earth" },
-    { id: 2, title: "Night arcade" },
-    { id: 3, title: "Soccer team VR" },
-    { id: 4, title: "The grid" },
-    { id: 5, title: "From up above VR" },
-    { id: 6, title: "Pocket borealis" },
-    { id: 7, title: "The curiosity" },
-    { id: 8, title: "Make it fisheye" }
+    { id: 1, title: "Deep earth", pic: deep },
+    { id: 2, title: "Night arcade", pic: night },
+    { id: 3, title: "Soccer team VR", pic: soccer },
+    { id: 4, title: "The grid", pic: grid },
+    { id: 5, title: "From up above VR", pic: above },
+    { id: 6, title: "Pocket borealis", pic: pocket },
+    { id: 7, title: "The curiosity", pic: curiosity },
+    { id: 8, title: "Make it fisheye", pic: fisheye }
   ];
   return (
     <div>
       <h2>Our creations</h2>
       <MyButton foo="See all" />
+      {projects.map(tile => (
+        <article key={tile.id}>
+          <img src={tile.pic} className="tile" />
+          <h3>{tile.title}</h3>
+        </article>
+      ))}
     </div>
   );
 }
 
 function MyButton(props) {
   return <button>{props.foo}</button>;
+}
+
+function Footer() {
+  const social = [
+    { id: 1, title: "Facebook", pic: facebook },
+    { id: 2, title: "Twitter", pic: twitter },
+    { id: 3, title: "Pinterest", pic: pinterest },
+    { id: 4, title: "Instagram", pic: instagram }
+  ];
+  return (
+    <footer>
+      <img src={logo} className="App-logo" alt="logo" />
+      {social.map(icon => (
+        <a href="#" key={social.id}>
+          <img src={logo.pic} className="social-icon" alt={logo.title} />
+        </a>
+      ))}
+      <NavLinks />
+      <p>© 2021 Loopstudios. All rights reserved.</p>
+    </footer>
+  );
 }
 
 export default App;
